3f992e0e1ecc9374b3730112937d140c5cea6b71
rajatgaikwad10 <105495831+rajatgaikwad10@users.noreply.github.com>
2023-03-22 12:23:11 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+model = pickle.load(open('AI_Sheet.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def man():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+
+    arr = np.array([[float(data1), float(data2), float(data3), float(data4),float(data5),float(data6)]])
+    pred = model.predict(arr)
+    return render_template('after.html', data=pred)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
